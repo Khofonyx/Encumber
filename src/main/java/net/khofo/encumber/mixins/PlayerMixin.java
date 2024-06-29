2af59fcf950bbb85d363936bb86554db374fc221
tjs325 <tjs325@lehigh.edu>
2024-06-28 22:13:15 -0400

    Cleaned Up Code

--- src/main/java/net/khofo/encumber/mixins/PlayerMixin.java
+++ src/main/java/net/khofo/encumber/mixins/PlayerMixin.java
@@ -3,17 +3,12 @@ package net.khofo.encumber.mixins;
 import net.khofo.encumber.configs.Configs;
 import net.khofo.encumber.events.WeightEvent;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LocalPlayer.class) // This annotation is crucial
+@Mixin(LocalPlayer.class)
 public abstract class PlayerMixin {
 
     static {
@@ -28,7 +23,7 @@ public abstract class PlayerMixin {
             double boostItemWeight = WeightEvent.getWeightWithBoostItem(player, 0);
             double threshold = WeightEvent.getThreshold(Configs.ENCUMBERED_THRESHOLD);
             if (playerWeight >= boostItemWeight && threshold > -1) {
-                cir.setReturnValue(false); // Cancel the sprinting if condition met
+                cir.setReturnValue(false);
             }
         }
     }
